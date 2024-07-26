208919f2c6beb63ca8af569f6fe00b5a950b8eaa
hyungwook0720 <hyung_ook@naver.com>
2024-07-27 03:35:36 +0900

    Update TestJava.java
    add "Hello github"

--- MyApplication/testjava/src/main/java/com/example/testjava/TestJava.java
+++ MyApplication/testjava/src/main/java/com/example/testjava/TestJava.java
@@ -4,6 +4,8 @@ public class TestJava {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello Python");
+
+        System.out.println("hello github");
     }
 
 }
