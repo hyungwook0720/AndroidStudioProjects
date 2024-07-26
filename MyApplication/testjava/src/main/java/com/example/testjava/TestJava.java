da42ce17df2aa9436f950db0d7351402ae6339e5
hyungwook0720 <hyung_ook@naver.com>
2024-07-27 03:32:22 +0900

    fix TestJava.java file

--- MyApplication/testjava/src/main/java/com/example/testjava/TestJava.java
+++ MyApplication/testjava/src/main/java/com/example/testjava/TestJava.java
@@ -3,6 +3,7 @@ package com.example.testjava;
 public class TestJava {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello Python");
     }
 
 }
